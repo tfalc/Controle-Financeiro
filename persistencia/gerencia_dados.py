d55a6f3dd8242cf2c414077317831c6e1177bfae
MRyukiimaru <46535838+Mryukiimaru@users.noreply.github.com>
2024-12-07 08:30:30 -0300

    Atualização e implementação de classe.
    Classes pra gerenciar a entrada de dados de receitas e despesas, uso de tkinter pra visualização da tabela de dados.

--- persistencia/gerencia_dados.py
+++
@@ -1,8 +0,0 @@
-import receita.calculos as calculos
-
-
-def salva_dados(dados):
-    with open("dados.csv", "a") as arquivo:
-        for dado in dados:
-            arquivo.write(f"{dado};")
-        arquivo.write("\n")
